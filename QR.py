ac21e8655f83159c545912a79d0213745351f790
Sabuhi Gasimzada <96132884+s3bu7i@users.noreply.github.com>
2022-08-24 19:05:41 +0400

    Add files via upload

---
+++ QR.py
@@ -0,0 +1,21 @@
+
+import urllib.parse
+import urllib.request
+
+
+try:
+    body = input("Enter the size of the QR code :")
+    data = input("Enter the information of the QR code to be generated :")
+    database = {
+        "size":body+"x"+body,
+        "data":data
+    }
+    database = urllib.parse.urlencode(database)
+    api_url = "https://api.qrserver.com/v1/create-qr-code/?"+database
+    document_name = ".\\QR_"+ data +".png"
+    urllib.request.urlretrieve(api_url,document_name)
+    print("Download is successful :)")
+except:
+    print("Error occured :(")
+
+    
